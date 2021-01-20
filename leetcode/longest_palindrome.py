752627d42af4518a9234cb3ccdc9e01a9f35dd87
Stuart Smith <stuartsayswhat@googlemail.com>
2021-01-20 18:53:30 +0000

    working code taken from discussions

--- leetcode/longest_palindrome.py
+++ leetcode/longest_palindrome.py
@@ -9,41 +9,59 @@ class Solution:
     def longestPalindrome(self, s):
 
 
-        # reverse s
-
+        # The code below doesn't work
+        """
         #rev_s = ""
         rev_s = ""
         copy_s = s
         word = ""
+        #counter_totals = []
+        counter_totals = {}
+        #counter_totals = set()
         
         for i in s[::-1]:
             rev_s += i
-        
-        #print(rev_s)
 
-        """
-        while copy_s.find(rev_s) != 0:
-            aa = len(rev_s) - 1
-            rev_s = rev_s[:aa]
-            bb = len(copy_s) -1
-            copy_s = copy_s[:bb]
-            #rev_rev = reversed(copy_s)
-            if copy_s == rev_s:
-                print(copy_s)
-                break
-            print(rev_s, copy_s)
-        """
-        for i in range(len(s)):
+        for i in range(len(rev_s)):
             word = s[i]
             counter = i
-            while len(word) < len(s) and s.find(word) == 0:
+            while counter < len(rev_s) -1  and rev_s.find(word) != -1:
                 counter += 1
-                word += s[i + counter]
+                word += s[counter]
+
+            counter_totals[len(word[:len(word)-1])] = word[:len(word)-1]
+        aa = max(counter_totals)
+        print(counter_totals[aa])
+        """
+
+        # The code below works - and was taken from discussions
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
+
 
 
 
 Run = Solution()
-Run.longestPalindrome("babad")
+Run.longestPalindrome("cbbd")
+
+("babad")
         
         
         
