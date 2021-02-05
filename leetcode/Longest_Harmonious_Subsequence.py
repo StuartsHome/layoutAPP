7db5dde6f87681de63392f69b3d78c13cb45de61
Stuart Smith <stuartsayswhat@googlemail.com>
2021-02-05 17:43:50 +0000

    harmony

--- leetcode/Longest_Harmonious_Subsequence.py
+++ leetcode/Longest_Harmonious_Subsequence.py
@@ -1,11 +1,22 @@
 class Solution:
     def findLHS(self, nums):
+        from collections import Counter
+
+        # code taken from discussions
+        count = Counter(nums)
+        ans = 0
+        for x in count:
+            if x+1 in count:
+                ans = max(ans, count[x] + count[x+1])
+        print(ans)
+
+
+        """
         carry = 0
         result = []
         temp = []
         a, j = 0,0
 
-        """
         for i in range(len(nums)):
             target = nums[i]
             a, j = i -1, i + 1
@@ -18,6 +29,7 @@ class Solution:
                 a -= 1
             if len(temp) > 1: result.append(len(temp))
             temp = []
+        """
         """
 
         for i in range(len(nums)):
@@ -34,10 +46,12 @@ class Solution:
                 a -= 1
             if len(temp) > 1: result.append(len(temp))
             temp = []
-
-
-
         print(result)
+        """
+
+
+
+
 
 
 Run = Solution()
