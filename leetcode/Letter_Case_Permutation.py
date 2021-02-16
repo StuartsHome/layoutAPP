b8d95370c3d690ca749de175c9f11049a2dafab1
Stuart Smith <stuartsayswhat@googlemail.com>
2021-02-16 17:39:38 +0000

    updated char permutation

--- leetcode/Letter_Case_Permutation.py
+++ leetcode/Letter_Case_Permutation.py
@@ -14,6 +14,17 @@ class Solution:
             res = tmp
         return res
 
+        # Backtracking process including List Comprehensions
+        """
+        res = ['']
+        for ch in S:
+            if ch.isalpha():
+                res = [i+j for i in res for j in [ch.upper(), ch.lower()]]
+            else:
+                res = [i+ch for i in res]
+        return res
+        """
+
                 
 
 Run = Solution()
